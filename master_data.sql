a6683db4c88c8b22096aea57420febb19944e476
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2023-12-17 06:37:19 +0900

    Insert products master data

---
+++ master_data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO products(id, name, description, price, available_stock)
+VALUES 
+('77cf3e11-c647-4aa9-81f2-460ae6a2fb3f', 'Pocky Strawberry', 'Pocky Strawberry 50g', 5.5, 15),
+('664235dc-be44-472c-b9f4-aca3bdc2763b', 'Kitkat Matcha', 'Kitkat Matcha', 5.0, 23),
+('be26640e-e242-43a0-91c0-fb285864729b', 'Kitkat Chocolate', 'Kitkat Chocolate', 4.0, 31),
+('74f8458f-1814-470e-811a-760232c5f146', 'Pringles Seaweed', 'Pringles Seaweed', 3.0, 27),
+('1fba01a9-43e7-41a0-b50a-6054a633dd73', 'Pringles Original', 'Pringles Original', 2.0, 43);
