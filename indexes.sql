1ed917e11b7c59b426048e946d2c1de0e3079ea0
dwinanto34 <87220725+dwinanto34@users.noreply.github.com>
2024-05-31 13:34:39 +0900

    Add unique and compound indexes example

--- indexes.sql
+++ indexes.sql
@@ -14,3 +14,24 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_order_items_product_id ON order_item
 DROP INDEX CONCURRENTLY IF EXISTS idx_order_items_order_id;
 DROP INDEX CONCURRENTLY IF EXISTS idx_order_items_order_item_id;
 DROP INDEX CONCURRENTLY IF EXISTS idx_order_items_product_id;
+
+-- Create unique index on products table
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_unique_products_name ON products(name);
+
+-- Drop unique indexes on products table
+DROP INDEX CONCURRENTLY IF EXISTS idx_unique_products_name;
+
+-- Create compound or composite indexes
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_products_name_description ON products(name, description);
+
+Queries that benefit from the index above:
+- SELECT * FROM products WHERE name = 'name' AND description = 'description';
+- SELECT * FROM products WHERE name = 'name';
+- SELECT * FROM products WHERE name = 'name' ORDER BY description;
+
+Queries that do not benefit from the index above:
+- SELECT * FROM products WHERE description = 'description';
+- SELECT * FROM products WHERE description = 'description' ORDER BY name;
+
+-- Drop compound or composite indexes
+DROP INDEX CONCURRENTLY IF EXISTS idx_products_name_description;
